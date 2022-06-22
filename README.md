db509e5fc4878abc1ab0a0db2102140aa0d5a27f
Shamanth_Kumar_H_P <shamanthkumarhp28@gmail.com>
2022-06-22 20:06:01 +0530

    video proccesing completed

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # TrafficAnalysisProject
+video
+
+pes
+frames = 239387
+time = 2:39:43
+startTime = 6:05:07
+startFrame = 7626
--- finalUrsRoad.py
+++ finalUrsRoad.py
@@ -1,25 +1,30 @@
 #%%
+#time complexity = 0.15*T average
 import cv2
 import numpy as np
 import datetime
+from datetime import datetime, timedelta
 import csv
 import time
+import calendar
 import matplotlib.pyplot as plt
+from collections import Counter
 
 days=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 intensity={ 1:"very low",2:"low", 3:"moderate", 4:"high", 5:"very high" }
 
 def generateDay():    
-    now = datetime.datetime.now()
+    #now = datetime.datetime.now()
     #day = now.strftime("%A")
     #date = now.strftime("%m/%d/%Y")
-    day = days[2]
-    date= '06/07/2022'
+    date='25-05-2022'
+    d = datetime.strptime(date, '%d-%m-%Y').weekday()
+    day = calendar.day_name[d]
     return day,date
 
 def feedDataset(data):
     #header = ['Date', 'Time','Day','Intensity','Comment']
-    with open(r'C:\Users\Shamanth kumar HP\Desktop\WebD\trafficAnalysis\dataSheetsCSV\Byappanahalli\camera1.csv', 'a', encoding='UTF8', newline='') as f:
+    with open(r'C:\Users\Shamanth kumar HP\Desktop\WebD\FinalYearProject\csvFiles\URS_road\camera1.csv', 'a', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
         #writer.writerow(header)    
         writer.writerows(data)       
@@ -47,10 +52,10 @@ offset=3      #10
 line_height=190#550
 matches =[]
 cnt=0
-'''
+FPS = 30
 #day
 cropTop = 380
-cropBottom = 1080
+cropBottom =720
 cropLeft = 300
 cropRight = 950
 '''
@@ -59,8 +64,10 @@ cropTop = 500
 cropBottom = 1080
 cropLeft = 500
 cropRight = 950
+'''
+cap = cv2.VideoCapture(r'C:\Users\Shamanth kumar HP\Desktop\WebD\FinalYearProject\shamanth\pes2.mp4')
+#1280*720p
 
-cap = cv2.VideoCapture(r'C:\Users\Shamanth kumar HP\Desktop\WebD\FinalYearProject\shamanth\pes7.mp4')
 if cap.isOpened():
     ret,frame0 = cap.read()
 else:
@@ -70,61 +77,63 @@ ret,frame2 = cap.read()
 
 frame1 = frame1[cropTop:cropBottom, cropLeft: cropRight]
 frame2 = frame2[cropTop:cropBottom, cropLeft:cropRight ]
-originalPic=cv2.imread(r'C:\Users\Shamanth kumar HP\Desktop\WebD\FinalYearProject\shamanth\darkRoad.jpg') #frame1 if empty
+originalPic=cv2.imread(r'C:\Users\Shamanth kumar HP\Desktop\WebD\FinalYearProject\shamanth\emptyRoad.jpg') #frame1 if empty
 #originalPic = frame0[cropTop:cropBottom, cropLeft: cropRight]
 len0,width0,_ = originalPic.shape
-print(originalPic.shape)
+#print(originalPic.shape)
 dimension = len0 * width0 #1280*720
 
 day, date = generateDay()
 fcount=1
-val=1
+temp=1
 totalList=[]
-tempTraffic=[]
-trafficSix=[]
+oneMinList=[]
+oneHourList = []
 feedList=[]
-#tsec=1
 startHour=6
-while fcount<2500:    
-    if fcount%25==0:
+while fcount<301:    
+    if fcount % FPS == 0:
         t=0
-        #print()
-        #print(fcount,"-",val)
-        if val<22:
+        if temp<22:
             t=1
             #print("very low traffic")
-        elif val <75:
+        elif temp <75:
             t=2
             #print("low traffic")
-        elif val <150:
+        elif temp <150:
             t=3
-        #print("moderate")
-        elif val <176:
+            #print("moderate")
+        elif temp <176:
             t=4
-        #print("high traffic")
+            #print("high traffic")
         else:
             t=5
             #print("very high traffic")
-        val=1
-        tempTraffic.append(t)
+        temp=1
+        oneMinList.append(t)
         
-    if fcount%125==0:
-        #print(tsec,"-",tempTraffic)
-        #trafficSix.append(max(tempTraffic))
-        #print(max(tempTraffic))
-        m = max(tempTraffic)
+    if fcount % (FPS*60) == 0:
+        #s = max(oneMinList, key = oneMinList.count)
+        s = Counter(oneMinList).most_common(1)[0][0]
+        #print(oneMinList)
+        oneHourList.append(s)
+        oneMinList=[]
+        print(fcount)
+     
+    if fcount % (FPS * 60 * 60) == 0:
+        #h = max(oneHourList, key = oneHourList.count)
+        h = Counter(oneHourList).most_common(1)[0][0]
         feedList.append(date)
         feedList.append(startHour)
         feedList.append(day)
-        feedList.append(m)
-        intense = intensity.get(m)
+        feedList.append(h)
+        intense = intensity.get(h)
         feedList.append(intense)
-        
+        print(feedList)
         totalList.append(feedList)
         
-        tempTraffic=[]
-        feedList=[]
-        #tsec+=1
+        oneHourList = []
+        feedList = []
         startHour+=1
     
     numpyOnes=0
@@ -142,8 +151,8 @@ while fcount<2500:
     contours,h = cv2.findContours(closing,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     for(i,c) in enumerate(contours):
         (x,y,w,h) = cv2.boundingRect(c)
-        contour_valid = (w >= min_contour_width) and (h >= min_contour_height)
-        if not contour_valid:
+        contour_tempid = (w >= min_contour_width) and (h >= min_contour_height)
+        if not contour_tempid:
             continue
         #cv2.rectangle(frame1,(x-10,y-10),(x+w+10,y+h+10),(255,0,0),2)
         
@@ -155,7 +164,7 @@ while fcount<2500:
             if y<(line_height+offset) and y>(line_height-offset) :            
                 matches.remove((x,y))                
                 cnt=cnt+1
-                val=int(val/2)
+                temp=int(temp/2)
                 t=True
               
     if t==False:
@@ -165,23 +174,23 @@ while fcount<2500:
             numpyOnes+=np.count_nonzero(i!=0)
            
         dens0 = int((numpyOnes / dimension)*1000) ##402*388  
-        #print("frame:",str(fcount)," -density= ",dens0,"-",val)
-        if dens0>750:
+        #print("frame:",str(fcount)," -density= ",dens0,"-",temp)
+        if dens0>700:
             pass #error in frame
         elif dens0<7 and dens==0:
             originalPic = np.array(frame1)
-        elif dens0>=400: #very high
-            val=val+8
+        elif dens0>=350: #very high
+            temp=temp+8
         elif dens0>=200: #high
-            val=val+4
+            temp=temp+4
         elif dens0>=100: #moderate
-            val=val+2
+            temp=temp+2
         elif dens0>=50: #low
-            val=val+1
+            temp=temp+1
         elif dens0>=15: #very low
             pass
        
-        #print("d frame:{0} val={1}  dens={2} ".format(str(fcount),str(val),str(dens0)))
+        #print("d frame:{0} temp={1}  dens={2} ".format(str(fcount),str(temp),str(dens0)))
         #cv2.imshow("dil0" , dilated0)
     #cv2.imshow("dil" , dilated)      
     #cv2.imshow("ref", originalPic)
@@ -196,17 +205,28 @@ while fcount<2500:
     
     if ret!=False:
         frame2 = frame2[cropTop:cropBottom, cropLeft: cropRight]
-        fcount+=1
+        fcount += 1
     #print(fcount)
         
     #time.sleep(0.05)
 #cv2.imwrite(r'C:\Users\Shamanth kumar HP\Desktop\WebD\FinalYearProject\shamanth\emptyRoad.jpg',originalPic)    
 print("total",cnt)   
 print("frame-",fcount)
-print(totalList)
-#csv    
-feedDataset(totalList) 
 
+#csv 
+if oneHourList != []:
+    feedList.append(date)
+    feedList.append(startHour)
+    feedList.append(day)
+    #h = max(oneHourList, key = oneHourList.count) #for last remaining minutes != 60
+    h = Counter(oneHourList).most_common(1)[0][0]
+    feedList.append(h)
+    intense = intensity.get(h)
+    feedList.append(intense)
+
+    totalList.append(feedList)   
+feedDataset(totalList) 
+print(totalList)
 #csvToExcel('vehiclesCount.csv')   
    
 cv2.destroyAllWindows()
